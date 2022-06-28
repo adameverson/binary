06b7fc7de5b4830106e6bd59ad613a8797c9252b
adameverson <adameversons@gmail.com>
2022-06-28 18:06:55 -0300

    Analisys functions and arrays

---
+++ Windows_x64/analisys/arrays/int_vector.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+int main ()
+{
+	std::vector<int> meuVetor;
+	meuVetor.push_back(1);
+	meuVetor.push_back(2);
+	meuVetor.push_back(3);
+	meuVetor.push_back(4);
+	meuVetor.push_back(5);
+
+	return 0;
+}
